f913c52537607ff677a4e933e0ce5a063cf75bd3
Khalifa Muyideen <khalifa@Khalifas-MBP.hitronhub.home>
2023-05-30 00:54:43 -0500

    first commit

---
+++ migrations/versions/0d759a14a672_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 0d759a14a672
+Revises: 
+Create Date: 2023-05-29 17:25:05.980856
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0d759a14a672'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
